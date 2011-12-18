5ecb7a1f0b5f358f5ef7f0ebc7d24df7b17d436d
Stephen Poletto <stephenpoletto@gmail.com>
2011-12-18 14:42:58 -0500

    Updating launch chromes to contain the EC2 fleet. Uploading WC logs for 5, 10 clients.

--- distributed_testing/launchChromes.py
+++ distributed_testing/launchChromes.py
@@ -12,15 +12,33 @@
 
 from fabric.api import *
 
-env.user = "ubuntu"
-env.key_filename = "/Users/spoletto/.ssh/solvejs_client.pem"
-env.hosts = [
-    'ec2-107-22-73-187.compute-1.amazonaws.com',
+all_instances = [
+	'ec2-107-20-23-108.compute-1.amazonaws.com',
+	'ec2-184-73-44-173.compute-1.amazonaws.com',
+	'ec2-184-73-0-176.compute-1.amazonaws.com',
+	'ec2-107-22-93-180.compute-1.amazonaws.com',
+	'ec2-50-17-36-88.compute-1.amazonaws.com',
+	'ec2-174-129-96-234.compute-1.amazonaws.com',
+	'ec2-50-19-74-30.compute-1.amazonaws.com',
+	'ec2-174-129-91-238.compute-1.amazonaws.com',
+	'ec2-184-72-85-89.compute-1.amazonaws.com',
+	'ec2-107-22-7-170.compute-1.amazonaws.com',
+	'ec2-50-17-60-195.compute-1.amazonaws.com',
+	'ec2-184-73-121-244.compute-1.amazonaws.com',
+	'ec2-107-21-72-9.compute-1.amazonaws.com',
+	'ec2-75-101-195-77.compute-1.amazonaws.com',
+	'ec2-107-22-73-187.compute-1.amazonaws.com',
     'ec2-184-73-108-126.compute-1.amazonaws.com',
     'ec2-184-72-191-50.compute-1.amazonaws.com',
 	'ec2-50-16-112-185.compute-1.amazonaws.com',
 	'ec2-184-73-73-171.compute-1.amazonaws.com'
 ]
+
+INSTANCE_COUNT = 10
+
+env.user = "ubuntu"
+env.key_filename = "/Users/spoletto/.ssh/solvejs_client.pem"
+env.hosts = all_instances[0:INSTANCE_COUNT]
 
 @parallel
 def launch_chrome(server_address='http://ec2-184-72-170-12.compute-1.amazonaws.com:8080/'):
